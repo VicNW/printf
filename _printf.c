c44dcc01d33527fc695010961c3a8058b4fcf46c
Nwosu Victor <nwosuvictor54@gmail.com>
2022-05-17 23:58:45 +0400

    _printf

--- _printf.c
+++ _printf.c
@@ -1,31 +1,82 @@
-#include "main.h"
+#include "holberton.h"
+#include <stdlib.h>
+
 /**
- * _printf - function that prints based on format specifier
- * @format: takes in format specifier
- * Return: return pointer to index
+ * check_for_specifiers - checks if there is a valid format specifier
+ * @format: possible format specifier
+ *
+ * Return: pointer to valid function or NULL
+ */
+static int (*check_for_specifiers(const char *format))(va_list)
+{
+	unsigned int i;
+	print_t p[] = {
+		{"c", print_c},
+		{"s", print_s},
+		{"i", print_i},
+		{"d", print_d},
+		{"u", print_u},
+		{"b", print_b},
+		{"o", print_o},
+		{"x", print_x},
+		{"X", print_X},
+		{"p", print_p},
+		{"S", print_S},
+		{"r", print_r},
+		{"R", print_R},
+		{NULL, NULL}
+	};
+
+	for (i = 0; p[i].t != NULL; i++)
+	{
+		if (*(p[i].t) == *format)
+		{
+			break;
+		}
+	}
+	return (p[i].f);
+}
+
+/**
+ * _printf - prints anything
+ * @format: list of argument types passed to the function
+ *
+ * Return: number of characters printed
  */
 int _printf(const char *format, ...)
 {
-va_list valist;
-int count;
-format_t get_opt[] = {
-{"c", set_char},
-{"s", set_string},
-{"i", set_decimal},
-{"d", set_decimal},
-{"%", print_percent},
-{NULL, NULL}
-};
+	unsigned int i = 0, count = 0;
+	va_list valist;
+	int (*f)(va_list);
 
-if (!format)
-{
-return (-1);
-}
-
-va_start(valist, format);
-count = parse_format(format, get_opt, valist);
-va_end(valist);
-
-
-return (count);
+	if (format == NULL)
+		return (-1);
+	va_start(valist, format);
+	while (format[i])
+	{
+		for (; format[i] != '%' && format[i]; i++)
+		{
+			_putchar(format[i]);
+			count++;
+		}
+		if (!format[i])
+			return (count);
+		f = check_for_specifiers(&format[i + 1]);
+		if (f != NULL)
+		{
+			count += f(valist);
+			i += 2;
+			continue;
+		}
+		if (!format[i + 1])
+			return (-1);
+		_putchar(format[i]);
+		count++;
+		if (format[i + 1] == '%')
+			i += 2;
+		else
+			i++;
+	}
+	va_end(valist);
+	return (count);
 }
