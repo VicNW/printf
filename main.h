508064c4c66c1f6a20d6099f12f93e67d5b562d0
Nwosu Victor <nwosuvictor54@gmail.com>
2022-05-18 00:01:55 +0400

    _printf.c and main.h

--- main.h
+++ main.h
@@ -1,30 +1,32 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
 #include <stdarg.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
-int _printf(const char *format, ...);
-
 /**
- * struct format - This is a typedef struct.
- * @opt: Pointer type char.
- * @call_fun: Pointer to call the function.
+ * struct print - structure for printing various types
+ * @t: type to print
+ * @f: function to print
  */
-typedef struct format
+typedef struct print
 {
-char *opt;
-int (*call_fun)();
-} format_t;
+	char *t;
+	int (*f)(va_list);
+} print_t;
 
-int _printf(const char *format, ...);
-int parse_format(const char *format, format_t get_opt[], va_list valist);
-int set_string(va_list valist);
-int set_char(va_list valist);
 int _putchar(char c);
-int set_decimal(va_list valist);
-int print_percent(__attribute__((unused))va_list valist);
-int length_of_integer(int n);
-int get_length(char *str __attribute__((__unused__)), va_list valist);
-#endif
+int _printf(const char *format, ...);
+int print_c(va_list c);
+int print_s(va_list s);
+int print_i(va_list i);
+int print_d(va_list d);
+int print_u(va_list u);
+int print_b(va_list b);
+int print_o(va_list o);
+int print_x(va_list x);
+int print_X(va_list X);
+int print_p(va_list p);
+int print_S(va_list S);
+int print_r(va_list r);
+int print_R(va_list R);
+
+#endif  /* _MAIN_H */
