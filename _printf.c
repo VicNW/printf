290e5ac8ea846a33ef0c5dd4a0e587eb501efb2c
sent <sintayehutilahun89@yahoo.com>
2022-05-17 20:26:26 +0300

    written by Sintayehu

--- _printf.c
+++ _printf.c
@@ -1,45 +1,31 @@
 #include "main.h"
-#include <stdlib.h>
-
 /**
- * _printf - prints any string with certain flags for modification
- * @format: the string of characters to write to buffer
- * Return: an integer that counts how many writes to the buffer were made
+ * _printf - function that prints based on format specifier
+ * @format: takes in format specifier
+ * Return: return pointer to index
  */
 int _printf(const char *format, ...)
 {
-int i = 0, var = 0;
-va_list v_ls;
-buffer *buf;
+va_list valist;
+int count;
+format_t get_opt[] = {
+{"c", set_char},
+{"s", set_string},
+{"i", set_decimal},
+{"d", set_decimal},
+{"%", print_percent},
+{NULL, NULL}
+};
 
-buf = buf_new();
-if (buf == NULL)
+if (!format)
+{
 return (-1);
-if (format == NULL)
-return (-1);
-va_start(v_ls, format);
-while (format[i])
-{
-buf_wr(buf);
-if (format[i] == '%')
-{
-var = opid(buf, v_ls, format, i);
-if (var < 0)
-{
-i = var;
-break;
 }
-i += var;
-continue;
-}
-buf->str[buf->index] = format[i];
-buf_inc(buf);
-i++;
-}
-buf_write(buf);
-if (var >= 0)
-i = buf->overflow;
-buf_end(buf);
-va_end(v_ls);
-return (i);
+
+va_start(valist, format);
+count = parse_format(format, get_opt, valist);
+va_end(valist);
+
+
+return (count);
 }
--- main.h
+++ main.h
@@ -1,30 +1,30 @@
-#ifndef __MAIN_H__
-#define __MAIN_H__
-
+#ifndef MAIN_H
+#define MAIN_H
 #include <stdarg.h>
+#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+int _printf(const char *format, ...);
 
 /**
- * struct printer - format printer struct
- * @spec: the format specifier
- * @fn: the function that handles spec
+ * struct format - This is a typedef struct.
+ * @opt: Pointer type char.
+ * @call_fun: Pointer to call the function.
  */
-typedef struct printer
+typedef struct format
 {
-  char *spec;
-  int (*fn)(va_list);
-} print_t;
+char *opt;
+int (*call_fun)();
+} format_t;
 
-int _putchar(char c);
 int _printf(const char *format, ...);
-int print_char(va_list ap);
-int print_string(va_list ap);
-int print_space(va_list ap);
-int print_int(va_list ap);
-int print_unsigned(va_list ap);
-int print_octal(va_list ap);
-int print_digit(int num, int *count);
-int print_unsigned_digit(unsigned int num, int *count);
-int print_digit_octal(unsigned int num, int *count);
-int call_print_fn(char ch, va_list ap);
-#endif /* __MAIN_H__ */
+int parse_format(const char *format, format_t get_opt[], va_list valist);
+int set_string(va_list valist);
+int set_char(va_list valist);
+int _putchar(char c);
+int set_decimal(va_list valist);
+int print_percent(__attribute__((unused))va_list valist);
+int length_of_integer(int n);
+int get_length(char *str __attribute__((__unused__)), va_list valist);
+#endif
